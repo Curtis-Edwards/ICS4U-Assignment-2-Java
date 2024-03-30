703738b9ab31bd311c9a349a262f26dd78e71719
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-03-30 17:07:11 -0400

    Create MaxRun.java

---
+++ MaxRun.java
@@ -0,0 +1,79 @@
+/*
+ * Given a string, returns the length 
+ * of the largest run in the string.
+ *
+ * @author  Curtis Edwards
+ * @version 1.0
+ * @since   2024-03-06
+ */
+
+import java.util.Scanner;
+
+/**
+* This is the standard "MaxRunFunction" program.
+*/
+final class MaxRunFunction {
+
+    /**
+    * Prevent instantiation.
+    * Throw an exception IllegalStateException.
+    * if this is ever called
+    *
+    * @throws IllegalStateException if this is ever called
+    *
+    */
+    private MaxRunFunction() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * This function calculates the max run of a string.
+    *
+    * @param userString The user's input.
+    *
+    * @return maxRun The max run of the string
+    */
+    public static String maxRunFunction(String userString) {
+        int maxRun = 1;
+        int currentRun = 1;
+
+        for (int i = 0; i < userString.length() - 1; i++) {
+            if (userString.charAt(i) == userString.charAt(i + 1)) {
+                currentRun++;
+                if (currentRun > maxRun) {
+                    maxRun = currentRun;
+                }
+            } else {
+                currentRun = 1;
+            }
+        }
+        return maxRun;
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+    public static void main(String[] args) {
+       // Input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String userString = scanner.nextLine().trim();
+        
+        // Error check
+        if (userString.length() == 0) {
+            System.out.println("Invalid input.");
+        } else {
+            // Process
+            int maxRun = maxRunFunction(userString);
+            
+            // Output
+            System.out.print("The string \"" + userString);
+            System.out.println("\" has a max run of " + maxRun + "!");
+        }
+        
+        System.out.println("\nDone.");
+
+    }
+}
